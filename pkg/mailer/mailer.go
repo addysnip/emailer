91d1da0596eabc969b491d70e22c41643086e99f
Daniel Hawton <daniel@hawton.org>
2021-12-25 15:05:29 -0700

    Add ability to pass from information

--- pkg/mailer/mailer.go
+++ pkg/mailer/mailer.go
@@ -17,6 +17,7 @@ type Payload struct {
 	Template  string                 `json:"template"`
 	Data      map[string]interface{} `json:"data"`
 	Recipient string                 `json:"recipient"`
+	From      string                 `json:"from"`
 }
 
 var log = logger.Category("pkg/mailer")
@@ -38,7 +39,7 @@ func Handle(body string) error {
 		return err
 	}
 
-	err = Send(payload.Recipient, tem.Subject, BuildBody(payload.Recipient, tem.Subject, out))
+	err = Send(payload.Recipient, tem.Subject, BuildBody(payload.From, payload.Recipient, tem.Subject, out))
 	if err != nil {
 		return err
 	}
@@ -77,17 +78,20 @@ func BuildTemplate(tmpl models.Template, name string, data map[string]interface{
 	return out, nil
 }
 
-func BuildBody(to string, subject string, body *bytes.Buffer) []byte {
-	msg := []byte("To: " + to + "\r\n" +
-		"From: AddySnip <no-reply@addysnip.com>\r\n" +
-		"Subject: " + subject + "\r\n" +
-		"MIME-version: 1.0;\r\n" +
-		"Content-Type: text/html; charset=\"UTF-8\";\r\n" +
-		"\r\n" +
-		body.String(),
-	)
+func BuildBody(from string, to string, subject string, body *bytes.Buffer) []byte {
+	var msg string
+	msg = "To: " + to + "\r\n"
+	if from != "" {
+		msg += "From: " + from + "\r\n"
+	} else {
+		msg += "From: AddySnip <no-reply@addysnip.com>\r\n"
+	}
+	msg += "Subject: " + subject + "\r\n"
+	msg += "MIME-Version: 1.0\r\n"
+	msg += "Content-Type: text/html; charset=\"UTF-8\"\r\n"
+	msg += "\r\n" + body.String()
 
-	return msg
+	return []byte(msg)
 }
 
 func Send(to string, subject string, body []byte) error {
